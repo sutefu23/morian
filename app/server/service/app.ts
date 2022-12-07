6f2f59320c1cb74b7f2bd2e3fd0a933cebc61115
sutefu23 <sutefu23@gmail.com>
2022-12-07 13:20:36 +0900

    CORS設定追加

--- app/server/service/app.ts
+++ app/server/service/app.ts
@@ -2,14 +2,14 @@ import Fastify, { FastifyServerFactory } from 'fastify'
 import helmet from 'fastify-helmet'
 import cors from 'fastify-cors'
 import fastifyJwt from 'fastify-jwt'
-import { API_JWT_SECRET, API_BASE_PATH, NODE_ENV } from '$/envValues'
+import { HOST, API_JWT_SECRET, API_BASE_PATH, NODE_ENV } from '$/envValues'
 import server from '$/$server'
 
 export const init = (serverFactory?: FastifyServerFactory) => {
   const app = Fastify({ serverFactory })
   console.log('環境:' + NODE_ENV)
   app.register(helmet)
-  app.register(cors)
+  app.register(cors,{origin:HOST})
 
   app.addHook('onError', (req, reply, err) => {
     console.error('onError')
